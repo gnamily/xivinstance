2d78777fdea839fea759c44e5463d870d813a761
gnamily <gnamily@gmail.com>
2024-07-21 00:49:40 +0200

    init

---
+++ InstanceInfos/Plugin.cs
@@ -0,0 +1,40 @@
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using Dalamud.Game.Gui.Dtr;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
+using Dalamud.Game.Text;
+
+namespace InstanceInfos;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] internal static IFramework Framework { get; private set; } = null!;
+
+    private const string CommandName = "/instanceinfos";
+
+
+    public readonly WindowSystem WindowSystem = new("Instance Infos");
+    private IDtrBarEntry dtrEntry;
+
+    public Plugin(IDalamudPluginInterface pluginInterface, IDtrBar dtrBar)
+    {
+        this.dtrEntry = dtrBar.Get("instanceinfos");
+        Framework.Update += UpdateInstanceInfos;
+    }
+    public unsafe void UpdateInstanceInfos(IFramework framework)
+    {
+        uint instance = UIState.Instance()->PublicInstance.InstanceId;
+        uint iconNumber = (int)SeIconChar.Instance1 + (instance - 1);
+        SeIconChar icon = (SeIconChar)(iconNumber);
+        this.dtrEntry.Text = $" {icon.ToIconChar()}";
+    }
+
+    public void Dispose()
+    {
+        this.dtrEntry.Remove();
+        Framework.Update -= UpdateInstanceInfos;
+    }
+}
